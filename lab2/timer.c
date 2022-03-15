bfe539da99929660117b04ecd36cd543e357a7a8
João António Pereira <up202007145@fe.up.pt>
2022-03-15 17:28:13 +0000

    Implement timer_get_conf

--- lab2/timer.c
+++ lab2/timer.c
@@ -32,10 +32,9 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  sys_outb(TIMER_CTRL, TIMER_RB_CMD | TIMER_RB_SEL(timer));
 
-  return 1;
+  return util_sys_inb(TIMER_0 + timer, st);
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
