a86ebbca83b0e6a04bb5a62db79b68185d69294f
Nuno Pereira <up202007865@fe.up.pt>
2022-03-15 17:08:13 +0000

    Corrected MSB and LSB functions

--- lab2/utils.c
+++ lab2/utils.c
@@ -3,12 +3,12 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  *lsb = (uint8_t) (val & 0x0F);
+  *lsb = (uint8_t) (val & 0x00FF);
   return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  *msb = (uint8_t) (val & 0xF0);
+  *msb = (uint8_t) (val & 0xFF00);
   return 0;
 }
 
