0d4e94cdf5b884fbd2a72adbd2b7ff12e7edb781
Nuno Pereira <up202007865@fe.up.pt>
2022-03-15 17:09:29 +0000

    Corrected MSB function

--- lab2/utils.c
+++ lab2/utils.c
@@ -8,7 +8,7 @@ int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  *msb = (uint8_t) (val & 0xFF00);
+  *msb = (uint8_t) ((val & 0xFF00) >> 8);
   return 0;
 }
 
