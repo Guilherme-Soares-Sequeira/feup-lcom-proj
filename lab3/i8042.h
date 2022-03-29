5a964c4c90690c33cb29625ebac8e76ef48e80ac
Guilherme Sequeira <up202004648@fe.up.pt>
2022-03-29 16:30:27 +0100

    Added out and in buffers' macros

--- lab3/i8042.h
+++ lab3/i8042.h
@@ -5,6 +5,9 @@
 
 #define KBC_IRQ 1 /**< @brief KBC IRQ line */
 
-
+#define KBC_IN_BUF_DATA 0x60
+#define KBC_IN_BUF_CMD 0x64
+#define KBC_OUT_BUF_SCAN 0x60
+#define KBC_OUT_BUF_STATUS 0x64
 
 #endif
