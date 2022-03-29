9ec8b65e74601120511fb24a23096c8d0ba51f83
Guilherme Sequeira <up202004648@fe.up.pt>
2022-03-29 16:47:45 +0100

    Added kbc subscribe function.

--- lab3/kbc.c
+++ lab3/kbc.c
@@ -4,3 +4,22 @@
 #include "i8042.h"
 
 #define DELAY_US 20000
+
+uint8_t global_hook_id;
+
+int32_t used_ids = 0;
+
+int (kbc_subscribe_int)(uint8_t *bit_no) {
+  int hook_id; // random
+
+  while (BIT(hook_id = (rand() % 32)) & used_ids);
+  used_ids |= BIT(hook_id);
+
+  *bit_no = (uint8_t) hook_id;
+
+  int res = sys_irqsetpolicy(KBC_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &hook_id);
+
+  global_hook_id = (uint8_t) hook_id;
+
+  return res;
+}
