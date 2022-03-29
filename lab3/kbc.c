97abe45cb6b2e58197b17cc3f3d6bd8fbde80f76
Nuno Pereira <up202007865@fe.up.pt>
2022-03-29 16:55:29 +0100

    Added interrupt unsubscribe

--- lab3/kbc.c
+++ lab3/kbc.c
@@ -9,10 +9,11 @@ uint8_t global_hook_id;
 
 int32_t used_ids = 0;
 
-int (kbc_subscribe_int)(uint8_t *bit_no) {
+int(kbc_subscribe_int)(uint8_t *bit_no) {
   int hook_id; // random
 
-  while (BIT(hook_id = (rand() % 32)) & used_ids);
+  while (BIT(hook_id = (rand() % 32)) & used_ids)
+    ;
   used_ids |= BIT(hook_id);
 
   *bit_no = (uint8_t) hook_id;
@@ -22,4 +23,8 @@ int (kbc_subscribe_int)(uint8_t *bit_no) {
   global_hook_id = (uint8_t) hook_id;
 
   return res;
+}
+
+int(kbc_unsubscribe_int)(uint8_t *bit_no) {
+  return sys_irqrmpolicy((int *) &global_hook_id);
 }
