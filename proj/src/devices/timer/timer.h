d54f4c49820da3fbd89cda74055bc8be12943683
Nuno Pereira <nunoafonso2002@gmail.com>
2022-06-12 14:46:50 +0100

    Added documentation for timer, rtc, mouse and keyboard

--- proj/src/devices/timer/timer.h
+++ proj/src/devices/timer/timer.h
@@ -3,6 +3,11 @@
 
 #include <lcom/lcf.h>
 
+/**
+ * @brief get the current counter valur for the timer
+ * 
+ * @return the current counter value
+ */
 unsigned long (get_timer_get_counter)();
 
 #endif /* _LCOM_I8254_H */
